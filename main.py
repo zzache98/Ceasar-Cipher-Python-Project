d64be0eb7e04d5baf0c0046183511dfacb552091
zzache98 <82834837+zzache98@users.noreply.github.com>
2023-08-16 09:02:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+from art import logo
+print(logo)
+
+Game_ended = False
+
+while not Game_ended:
+  user_restart_choice = input("Would you like to restart the cipher program?")
+  if user_restart_choice == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift  % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    
+  elif user_restart_choice == "no":
+    Game_ended = True
+    print("Alrighty then.")
+    
+  
+
+
